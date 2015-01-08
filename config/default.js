14168c1b143fc85c1cd50dcadf5513420d2bf442
Michael Novotny <manovotny@gmail.com>
2015-01-08 00:10:26 -0600

    Adds project structure.

---
+++ config/default.js
@@ -0,0 +1,35 @@
+module.exports = (function () {
+
+    'use strict';
+
+    return {
+        author: {
+            email: 'manovotny@gmail.com',
+            name: 'Michael Novotny',
+            url: 'http://manovotny.com',
+            username: 'manovotny'
+        },
+        files: {
+            browserify: 'bundle'
+        },
+        paths: {
+            curl: 'curl_downloads',
+            source: 'src',
+            translations: 'lang'
+        },
+        project: {
+            composer: {
+                name: 'manovotny/williambutlerart',
+                type: 'project' // Should be `library` or `project`.
+            },
+            description: 'A customized landing page for williambutlerart.com.',
+            git: 'git://github.com/manovotny/williambutlerart.com.git',
+            name: 'William Butler Art',
+            slug: 'williambutlerart',
+            type: 'theme', // Should be `plugin` or `theme`.
+            url: 'https://github.com/manovotny/williambutlerart.com',
+            version: '0.0.0'
+        }
+    };
+
+}());
---
+++ config/replace.js
@@ -0,0 +1,30 @@
+module.exports = (function () {
+
+    'use strict';
+
+    return {
+        author: {
+            email: 'manovotny@gmail.com',
+            name: 'Michael Novotny',
+            url: 'http://manovotny.com',
+            username: 'manovotny'
+        },
+        project: {
+            composer: {
+                name: 'manovotny/williambutlerart',
+                type: 'project'
+            },
+            description: 'A customized landing page for williambutlerart.com.',
+            git: 'git://github.com/manovotny/williambutlerart.com.git',
+            name: 'William Butler Art',
+            slug: 'williambutlerart',
+            url: 'https://github.com/manovotny/williambutlerart.com',
+            version: '0.0.0'
+        },
+        translations: {
+            domain: 'williambutlerart',
+            path: 'lang'
+        }
+    };
+
+}());
